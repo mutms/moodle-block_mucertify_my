e1a541ad6ff9de5c02f44c963cd13fab71d63638
Petr Skoda <petr@mutms.org>
2025-04-29 06:49:42 +0200

    release mu-4.5.4-08

--- version.php
+++ version.php
@@ -30,12 +30,12 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'block_mucertify_my';
-$plugin->version   = 2025042800;
+$plugin->version   = 2025042900;
 $plugin->requires  = 2024091700.00;
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->supported = [405, 405];
-$plugin->release   = 'mu-4.5.4-07';
+$plugin->release   = 'mu-4.5.4-08';
 
 $plugin->dependencies = [
-    'tool_mucertify' => 2025042800,
+    'tool_mucertify' => 2025042900,
 ];
